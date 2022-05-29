448f2d2f495b011c84337b2dfc5fa711a6bd729c
MarleneOD <52855380+MarleneOD@users.noreply.github.com>
2022-05-29 17:30:33 +0200

    ultimo

--- Prueba_hola_mundo/src/Ejercicios_basicos/Ejercicio10.java
+++ Prueba_hola_mundo/src/Ejercicios_basicos/Ejercicio10.java
@@ -1,5 +1,6 @@
 package Ejercicios_basicos;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Ejercicio10{
 	
@@ -10,6 +11,28 @@ public class Ejercicio10{
 			o Mostrar qué color sale hasta que aparezca la primera azul (incluida la azul)
 		 
 		 */
+		
+		int azul=0;
+		int rojo=1;
+		int verde=2;
+		int bolaAzul=0;
+		
+		for(int i=0; i<10; i++) {
+			int r= (int) Math.floor(Math.random()*3);
+			if(r==0) {
+				bolaAzul+=1;
+				System.out.println("azul");
+				break;
+			}else if(r==1) {
+				System.out.println("rojo");
+			}else {
+				System.out.println("verde");
+			}
+			if(r==0) {
+			bolaAzul+=1;
+			}
+		}
+		System.out.println(bolaAzul);
 		
 		
 	}
